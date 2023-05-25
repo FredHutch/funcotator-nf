db5977f10fb69df6c9f9c1aa4dcfd723c6061ae0
Sam Minot <sminot@fredhutch.org>
2023-05-25 12:51:09 -0700

    Gzip outputs

--- templates/funcotator.sh
+++ templates/funcotator.sh
@@ -2,13 +2,19 @@
 
 set -e
 
+OUTPUT="${variant_vcf.replaceAll(/.vcf/, '')}.funcotated.${params.output_file_format.toLowerCase()}"
+echo "Writing to \${OUTPUT}"
+
 ./gatk Funcotator \
      --variant "${variant_vcf}" \
      --reference "${reference_fasta}" \
      --ref-version "${ref_version}" \
      --data-sources-path "${data_sources}" \
-     --output "${variant_vcf.replaceAll(/.vcf/, '')}.funcotated.${params.output_file_format.toLowerCase()}" \
+     --output "\${OUTPUT}" \
      --output-file-format "${params.output_file_format}" \
      --ignore-filtered-variants "${params.ignore_filtered_variants}" \
      --transcript-selection-mode "${params.transcript_selection_mode}" \
      --allow-hg19-gencode-b37-contig-matching ${params.allow_hg19_gencode_b37_contig_matching}
+
+echo Compressing output
+gzip \${OUTPUT}
